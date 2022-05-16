33c34c4d80e74a125821c1edf18e0fd442b8bcc0
Bianca Dias Bezerra <biancakiera5@gmail.com>
2022-05-16 09:27:54 -0300

    animal

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ANIMAL
+          ANIMALdasdasdasd
         </p>
         <a
           className="App-link"
